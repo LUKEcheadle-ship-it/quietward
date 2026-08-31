5c2551245539883e2facdf2ec8812b6ad133b88a
LUKEcheadle-ship-it <LUKEcheadle@icloud.com>
2026-08-31 18:26:14 -0500

    feat: expose sanitized Response handoff builder

--- src/quietward/integrations/__init__.py
+++ src/quietward/integrations/__init__.py
@@ -2,9 +2,12 @@
 
 from .forge import build_forge_explanation_request, validate_forge_explanation_response
 from .ollama import OllamaIncidentExplainer
+from .response import RESPONSE_CONTEXT_VERSION, build_response_handoff_events
 
 __all__ = [
     "OllamaIncidentExplainer",
+    "RESPONSE_CONTEXT_VERSION",
     "build_forge_explanation_request",
+    "build_response_handoff_events",
     "validate_forge_explanation_response",
 ]
